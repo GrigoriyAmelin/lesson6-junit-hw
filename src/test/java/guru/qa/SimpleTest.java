c4582c0766b840d58d19438d8048edd3cb4d12fb
amelin <g-amelin@mail.ru>
2022-03-16 01:37:13 +0300

    initial commit

---
+++ src/test/java/guru/qa/SimpleTest.java
@@ -0,0 +1,14 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleTest {
+
+    @Test
+    void simpleTest() {
+        assertTrue(true);
+
+    }
+}
