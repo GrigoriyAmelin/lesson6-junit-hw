ac29812689314ab5e3ebf6049f140a1200bc1d1f
amelin <g-amelin@mail.ru>
2022-04-02 17:09:35 +0300

    Added divisionFmsSearchWithInts void in ParametrizedTest

--- src/test/java/guru/qa/AThreeInvoiceSearch.java
+++ src/test/java/guru/qa/AThreeInvoiceSearch.java
@@ -1,5 +1,6 @@
 package guru.qa;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +16,7 @@ public class AThreeInvoiceSearch {
     @BeforeEach
     void precondition() {
 
+        Configuration.browserSize = "1920x1080";
         Selenide.open("https://www.a-3.ru/payment/6887");
         $("#number").scrollTo().click();
         sleep(2000);
@@ -39,6 +41,20 @@ public class AThreeInvoiceSearch {
                 .parent().shouldHave(text("3"));
         $("[data-testid='UserDataForm-Section-Inputs']").$(byText("Номер подразделения"))
                 .parent().shouldHave(text(testData));
+        sleep(1000);
+    }
+
+    @ValueSource(ints = {1145019, 1145025})
+    @ParameterizedTest(name = "Проверка ввода корректного номера подразделения ФМС \"{0}\"")
+    void divisionFmsSearchWithInts(int testData) {
+
+        $("[data-testid='Input']").scrollTo().setValue(String.valueOf(testData));
+        sleep(1000);
+        $("[data-testid='UserDataForm-SubmitButton']").click();
+        $("[data-testid='UserDataForm-Section-Inputs']").$(byText("Услуга"))
+                .parent().shouldHave(text("3"));
+        $("[data-testid='UserDataForm-Section-Inputs']").$(byText("Номер подразделения"))
+                .parent().shouldHave(text(String.valueOf(testData)));
         sleep(1000);
     }
 }
